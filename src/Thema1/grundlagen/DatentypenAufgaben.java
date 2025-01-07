c86d804dad68a3a3312fbf6cc4fd33fcf787e0cd
unknown <s.sluschny@web.de>
2025-01-07 14:51:44 +0100

    added remaining tasks

--- src/Thema1/grundlagen/DatentypenAufgaben.java
+++ src/Thema1/grundlagen/DatentypenAufgaben.java
@@ -20,11 +20,25 @@ public class DatentypenAufgaben {
 		boolean b = true;
 		System.out.println("bool = " + b);
 		
+		int num1 = 10;
+		int num2 = 3;
+		
+		int addition = num1 + num2;
+		int subtraktion = num1 - num2;
+		int division = num1 / num2;
+		int modulo = num1 % num2;
+		
+		System.out.println("Werte: " + num1 + " und " + num2);
+		System.out.println("Addition: " + addition);
+		System.out.println("Subtraktion: " + subtraktion);
+		System.out.println("Division: " + division);
+		System.out.println("Modulo: " + modulo);
+		
 		additionAufgabe(3,2);
 		
 	}
 
-	//Rechen-Operationen
+	//Rechen-Operationen eigene Funktion
 	
 	public static int additionAufgabe(int zahl1, int zahl2) {
 		int ergebnis = zahl1 + zahl2;
---
+++ src/Thema1/grundlagen/charZeichenketten.java
@@ -0,0 +1,101 @@
+package Thema1.grundlagen;
+
+public class charZeichenketten {
+
+	public static void main(String[] args) {
+		
+		// Aufgabe 2.5.2 und 2.5.3
+		
+		System.out.println("Zeichen\t\tASCII-Wert");
+		
+		String helloWorld = "Hello world!";
+		char char1h = helloWorld.charAt(0);
+		int zeichen1 = (int) char1h;
+		System.out.println("H \t\t" + zeichen1);
+		
+		char char2e = helloWorld.charAt(1);
+		int zeichen2 = (int) char2e;
+		System.out.println("e \t\t" + zeichen2);
+		
+		char char3l = helloWorld.charAt(2);
+		int zeichen3 = (int) char3l;
+		System.out.println("l \t\t" + zeichen3);
+		
+		char char4l = helloWorld.charAt(3);
+		int zeichen4 = (int) char4l;
+		System.out.println("l \t\t" + zeichen4);
+		
+		char char5o = helloWorld.charAt(4);
+		int zeichen5 = (int) char5o;
+		System.out.println("o \t\t" + zeichen5);
+		
+		char char6leer = helloWorld.charAt(5);
+		int zeichen6 = (int) char6leer;
+		System.out.println(" \t\t" + zeichen6);
+		
+		char char7w = helloWorld.charAt(6);
+		int zeichen7 = (int) char7w;
+		System.out.println("w \t\t" + zeichen7);
+		
+		char char8o = helloWorld.charAt(7);
+		int zeichen8 = (int) char8o;
+		System.out.println("o \t\t" + zeichen8);
+		
+		char char9r = helloWorld.charAt(8);
+		int zeichen9 = (int) char9r;
+		System.out.println("r \t\t" + zeichen9);
+		
+		char char10l = helloWorld.charAt(9);
+		int zeichen10 = (int) char10l;
+		System.out.println("l \t\t" + zeichen10);
+		
+		char char11d = helloWorld.charAt(10);
+		int zeichen11 = (int) char11d;
+		System.out.println("d \t\t" + zeichen11);
+		
+		char char12ex = helloWorld.charAt(11);
+		int zeichen12 = (int) char12ex;
+		System.out.println("! \t\t" + zeichen12);
+		
+		
+		// Aufgabe 2.5.1
+		char charh = 'H';
+		char chare = 'e';
+		char charl = 'l';
+		char charo = 'o';
+		char charleer = ' ';
+		char charw = 'w';
+		char charr = 'r';
+		char chard = 'd';
+		char charex = '!';
+		
+		int buchstabe1 = (int) charh;
+		System.out.println(buchstabe1);
+		int buchstabe2 = (int) chare;
+		System.out.println(buchstabe2);
+		int buchstabe3 = (int) charl;
+		System.out.println(buchstabe3);
+		int buchstabe4 = (int) charl;
+		System.out.println(buchstabe4);
+		int buchstabe5 = (int) charo;
+		System.out.println(buchstabe5);
+		int buchstabe6 = (int) charleer;
+		System.out.println(buchstabe6);
+		int buchstabe7 = (int) charw;
+		System.out.println(buchstabe7);
+		int buchstabe8 = (int) charo;
+		System.out.println(buchstabe8);
+		int buchstabe9 = (int) charr;
+		System.out.println(buchstabe9);
+		int buchstabe10 = (int) charl;
+		System.out.println(buchstabe10);
+		int buchstabe11 = (int) chard;
+		System.out.println(buchstabe11);
+		int buchstabe12 = (int) charex;
+		System.out.println(buchstabe12);
+		
+		
+
+	}
+
+}
